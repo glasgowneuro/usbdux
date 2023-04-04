4a11b5ef43bed1d8d040ba2430a534e51fce6e70
Bernd Porr <mail@berndporr.me.uk>
2023-04-04 15:19:23 +0100

    Added a try/catch statement

--- cpp-usbdux/print2screen_demo.cpp
+++ cpp-usbdux/print2screen_demo.cpp
@@ -13,8 +13,14 @@ struct DUXCallback : CppUSBDUX::Callback {
 int main (int,char**) {
 	CppUSBDUX usbdux;
 	DUXCallback cb;
-	usbdux.open();
-	usbdux.start(&cb,8,250);
+	try {
+		usbdux.open();
+		usbdux.start(&cb,8,250);
+	} catch (const char* msg) {
+		fprintf(stderr,"%s\n",msg);
+		fprintf(stderr,"Check with 'sudo dmesg' if the USBDUX driver has been loaded.\n");
+		exit(-1);
+	}
 	getc(stdin);
 	usbdux.stop();
 	return 0;
